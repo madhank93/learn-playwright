149b3cc7f12c1ce89c90b25d8eae14b28e1c82eb
madhank93 <maddymadhan946@gmail.com>
2021-10-04 22:17:09 +0530

    Initial commit 🚀🚀🚀

---
+++ tests/foo.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect, BrowserContext } from "@playwright/test";
+
+test("basic test", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  const title = page.locator(".navbar__inner .navbar__title");
+  await expect(title).toHaveText("Playwright");
+});
